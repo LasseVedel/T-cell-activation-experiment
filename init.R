69b7333b6886376c1ed2558dfc73103745fa39dd
LasseVedel <31420059+LasseVedel@users.noreply.github.com>
2022-02-27 22:36:45 +0100

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "dplyr", "ggplot2", "tidyverse")
+my_packages = c("shiny", "dplyr", "ggplot2", "tidyverse", "MatrixGenerics")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {

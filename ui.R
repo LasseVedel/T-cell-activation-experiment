09f7cb576579aa90267551267879cd947f684bfd
LasseVedel <31420059+LasseVedel@users.noreply.github.com>
2022-02-27 22:13:55 +0100

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+normalized_tibble_with_symbols <- read_csv("normalized_tibble_with_symbols.csv")
+matsymbol <- as.matrix(normalized_tibble_with_symbols[, 2:21])
+row.names(matsymbol) <-  normalized_tibble_with_symbols$...1
+gene_names <- sort(row.names(matsymbol))
+
+
+
+ui <- fluidPage(
+  selectInput("gene", "Please enter a gene of interest (Examples: IL2, CD28, LDLR):", gene_names),
+  plotOutput("plot")
+)

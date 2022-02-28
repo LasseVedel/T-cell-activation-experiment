ee3c12d9542e3026bc7079b6818bc219d639c6f0
LasseVedel <31420059+LasseVedel@users.noreply.github.com>
2022-02-28 13:03:34 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,6 +11,27 @@ gene_names <- sort(row.names(matsymbol))
 
 
 ui <- fluidPage(
-  selectInput("gene", "Please enter a gene of interest (Examples: IL2, CD28, LDLR):", gene_names),
-  plotOutput("plot")
+  
+  titlePanel("T cell activation experiment"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      selectInput("gene", "Please enter a gene of interest (Examples: IL2, CD28, LDLR):", gene_names),
+      p("Use this app to visualize gene expression data for activated T cells and T cells exposed to VSV-G pseudotyped lentivirus (LV)"),
+      p(""),
+      strong("Experimental design:"),
+      p("- 4 donors"),
+      p("- T cells stimulated with CD3/CD28 beads at cell:bead ratio of 1:1"),
+      p("- T cells kept at 1E6 cells/mL"),
+      p("- Lenti viral vector (VSV-G pseudotyped) added at MOI 10"),
+      p("- Total RNA extraction using RNeasy Mini Kit (Qiagen)"),
+      width = 4
+    ),
+    
+    mainPanel(
+      plotOutput("plot", width = "700px"),
+      downloadButton("dndPlot", "Download Graph")
+    )
+  )
 )

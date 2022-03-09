32c8f7ecf9f1b07cde8cc02a62f1a2026fd79432
LasseVedel <31420059+LasseVedel@users.noreply.github.com>
2022-03-09 18:20:55 +0100

    Update server.R

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ server <- function(input, output) {
     return(matsymbol)
   }) 
   
-  plotInput <- reactive({
+    plotInput1 <- reactive({
     ## The gene is found in the matsymbol to extract all normalized read counts 
     ## A matrix of dim=5x4 is formed 
     gene_counts <- t(matrix(DataInput()[input$gene,], nrow=4))
@@ -49,20 +49,63 @@ server <- function(input, output) {
       theme(axis.text.x = element_text(angle = 30, hjust=1))
     
     print(p_gene)
+    
   })
   
   
-  output$plot <- renderPlot({
-    print(plotInput())
-    ggsave("plot.png", plotInput(), dpi = 300)
+  plotInput2 <- reactive({
+    ## The gene is found in the matsymbol to extract all normalized read counts 
+    ## A matrix of dim=5x4 is formed 
+    gene_counts <- t(matrix(DataInput()[input$gene,], nrow=4))
+    values <- as.vector(t(gene_counts)) 
+    names <- c(rep("Non-stimulated", 4),rep("Stimulated, 24h", 4),rep("Stimulated, 48h", 4),rep("Stimulated, 48h + LV", 4),rep("Stimulated, 72h + LV", 4))
+    count_df <- data.frame(value = values, name = names)
+    
+    
+    b_gene <- ggplot(count_df, aes(x=names, y=values, fill = names)) +
+      geom_boxplot(color="#4d4d4d", lwd = 0.2) + 
+      geom_jitter(alpha=0.6, width=0.00, color = "#8c8c8c") + 
+      scale_fill_manual("Condition", 
+                        values = c("Non-stimulated" = "blue", 
+                                   "Stimulated, 24h" = "red", 
+                                   "Stimulated, 48h" = "green",
+                                   "Stimulated, 48h + LV" = "yellow",
+                                   "Stimulated, 72h + LV" = "black")) + 
+      labs(x="Condition", y = "Normalized expression (read counts)") +
+      ggtitle(label = paste(input$gene, "expression")) + 
+      theme(plot.title = element_text(color = "black", size = 12, face = "bold", hjust = 0.5)) + 
+      theme(axis.text.x = element_text(angle = 30, hjust=1))
+    
+    print(b_gene)
+    
   })
   
-  output$dndPlot <- downloadHandler(
+  
+  output$plot1 <- renderPlot({
+    print(plotInput1())
+    ggsave("plot1.png", plotInput1(), dpi = 300)
+  })
+  
+  output$plot2 <- renderPlot({
+    print(plotInput2())
+    ggsave("plot2.png", plotInput2(), dpi = 300)
+  })
+  
+  output$dndPlot1 <- downloadHandler(
     filename = function() {
-      "plot.png"
+      "plot1.png"
     },
     content = function(file) {
-      file.copy("plot.png", file, overwrite=TRUE)
+      file.copy("plot1.png", file, overwrite=TRUE)
     }
-  ) 
+  )
+  
+  output$dndPlot2 <- downloadHandler(
+    filename = function() {
+      "plot2.png"
+    },
+    content = function(file) {
+      file.copy("plot2.png", file, overwrite=TRUE)
+    }
+  )
 }
